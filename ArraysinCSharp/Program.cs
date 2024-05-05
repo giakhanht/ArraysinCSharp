7e01630ce87a29e3dd6b5c85421a8f94b95f6ac4
giakhanht <giakhanht.cs@gmail.com>
2024-05-05 14:39:53 +0200

    Learn C# Array properties and methods

--- ArraysinCSharp/Program.cs
+++ ArraysinCSharp/Program.cs
@@ -10,6 +10,22 @@ namespace ArraysinCSharp
     {
         static void Main(string[] args)
         {
+            //An integer array
+            int[] anArray = { 21, 22, 23, 26, 32, 40 };
+
+            //Print out the length of an array
+            Console.WriteLine(anArray.Length);
+
+            //Copy the content of an array to another array
+            int[] anotherArray;
+            anotherArray = new[] { 1, 2, 3, 4 };
+            Array.Copy(anArray, anotherArray, 4);
+
+            //Sort the array numerically
+            Array.Sort(anArray);
+
+            //Determine if a certain value exists in an array
+            Array.IndexOf(anotherArray, anArray);
         }
     }
 }
